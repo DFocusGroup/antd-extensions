a0b287f6f451cd6e7cc1bf2075ba9edceb709a9c
hank_mj <987942918@qq.com>
2018-11-19 19:07:23 +0800

    feat: add the typings partly

---
+++ src/typings/InputDialog.d.ts
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+export interface InputDialogProps {
+  labels: {
+    title: string
+    subTitle?: string
+    placeholder?: string
+    confirm?: string
+    cancel?: string
+  }
+  value: string
+  maskClosable: boolean
+  visible: boolean
+  validator: Function
+  onConfirm(): void | Promise
+  onCancel: Function
+}
+
+export interface InputDialogState {}
+
+export default class InputDialog extends React.Component<InputDialogProps, InputDialogState> {
+  render(): JSX.Element | null
+}
